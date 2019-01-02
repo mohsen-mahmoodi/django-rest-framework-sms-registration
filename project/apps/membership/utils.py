7a7938cd1c1c51c37c2a3389f7ef95a8aef4927c
Mohsen Mahmoodi <mo.ttds.g@gmail.com>
2019-01-02 14:08:58 +0330

    Fix generate_pin to use django secret_key

--- project/apps/membership/utils.py
+++ project/apps/membership/utils.py
@@ -9,7 +9,7 @@ from .models import User
 def generate_pin(length=9):
     hash_algorithm = 'sha512'
     m = getattr(hashlib, hash_algorithm)()
-    m.update('secret-key'.encode('utf-8'))
+    m.update(getattr(settings, 'SECRET_KEY', None).encode('utf-8'))
     m.update(os.urandom(16))
     pin = str(int(m.hexdigest(), 16))[-length:]
     return pin
